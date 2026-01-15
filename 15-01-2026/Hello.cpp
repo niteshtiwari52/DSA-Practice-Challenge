d818442bf44a4826fa212509c709fe2b0827ad38
niteshtiwari52 <niteshtiwari5222@gmail.com>
2026-01-15 13:14:30 +0530

    C#1: setup complete

---
+++ 15-01-2026/Hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+
+    string name;
+    cin>>name;
+    cout<<name;
+
+    int num1, num2;
+    cin>>num1>>num2;
+    cout<<"\nSum is"<<num1+num2;
+    return 0;
+}
---
+++ 15-01-2026/testcasetemplate.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+    int n1, n2;
+
+    while(t--){
+        cin>>n1;
+        cin>>n2;
+
+        cout<<"sum is "<<n1+n2<<endl;
+
+    }
+
+    return 0;
+}
